36b2edf19827e4391a45d0b6ab68e7324a6acc40
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2022-03-02 20:59:53 +0200

    install ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout';
@@ -11,6 +15,13 @@ import { PagesModule } from './pages';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
 
     LayoutModule,
     PagesModule,
