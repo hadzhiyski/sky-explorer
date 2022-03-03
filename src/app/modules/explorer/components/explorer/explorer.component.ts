ea6a4bdcc0820102d2839c27f06831db2577da13
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2022-03-03 13:57:14 +0200

    use geolocation service to pan map to current location

--- src/app/modules/explorer/components/explorer/explorer.component.ts
+++ src/app/modules/explorer/components/explorer/explorer.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { circle, latLng, polygon, tileLayer } from 'leaflet';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { GeolocationService } from '@sky/geolocation';
+import { latLng, Map, tileLayer } from 'leaflet';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'sky-explorer',
@@ -7,6 +9,7 @@ import { circle, latLng, polygon, tileLayer } from 'leaflet';
   styleUrls: ['./explorer.component.scss'],
 })
 export class ExplorerComponent {
+  map: Map | undefined;
   options = {
     layers: [
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -14,7 +17,16 @@ export class ExplorerComponent {
         attribution: '...',
       }),
     ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909),
+    zoom: 17,
   };
+
+  constructor(private geolocation: GeolocationService) {
+    this.geolocation.pipe(take(1)).subscribe((pos) => {
+      this.map?.panTo(latLng(pos.coords.latitude, pos.coords.longitude));
+    });
+  }
+
+  onMapReady(map: Map) {
+    this.map = map;
+  }
 }
